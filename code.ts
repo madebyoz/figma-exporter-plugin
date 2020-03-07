b4417b7a8de4fefd0e5161de9b93aa828f30f2a7
Darwin D Wu <darwin67@users.noreply.github.com>
2020-03-07 12:42:36 -0800

    implement filename conventions
    - snake_case
    - camelCase

--- code.ts
+++ code.ts
@@ -1,7 +1,10 @@
+const SNAKE_CASE = 'snake_case';
+const CAMEL_CASE = 'camelCase';
+
 const CONVENTIONS = [
   'original',
-  'snake_case',
-  'camelCase'
+  SNAKE_CASE,
+  CAMEL_CASE
 ];
 
 // This shows the HTML page in "ui.html".
@@ -13,11 +16,50 @@ figma.showUI(__html__);
 figma.ui.onmessage = msg => {
   console.log(msg)
 
-  if (msg.type === 'load') {
+  switch (msg.type) {
+  case 'load':
     figma.ui.postMessage({ type: 'load', value: CONVENTIONS });
+    break;
+
+  case 'export':
+    const convention: string = msg.value;
+    exportAs(convention);
+    break;
+
+  default:
+    console.log('hello world');
+  }
+};
+
+function exportAs(convention: string) {
+  const filename = figma.root.name;
+
+  switch (convention) {
+  case SNAKE_CASE:
+    console.log(toSnakeCase(filename));
+    break;
+  case CAMEL_CASE:
+    console.log(toCamelCase(filename));
+    break;
+  default:
+    console.log(filename);
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  // figma.closePlugin();
-};
+  figma.closePlugin();
+}
+
+function toSnakeCase(value: string): string {
+  const regex = /[\s\W]+/g
+  return value.replace(regex, '_').toLowerCase();
+}
+
+function toCamelCase(value: string): string {
+  const result = value.split(/[\s\W]/g).map((str, idx) => {
+    return idx == 0 ? str.toLowerCase() : capitalize(str);
+  });
+  return result.join('');
+}
+
+function capitalize(value: string): string {
+  return value[0].toUpperCase() + value.substr(1).toLowerCase();
+}
