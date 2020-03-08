f2c9f7a76f40daca019b4c1b4e5d6ed96f68ab38
Darwin D Wu <darwin67@users.noreply.github.com>
2020-03-07 23:42:38 -0800

    setup jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testEnvironment: 'node',
+  testRegex: '.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true
+};
